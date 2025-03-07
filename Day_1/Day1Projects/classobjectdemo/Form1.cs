6a4d5e77fe63f539edad6d334b285c717816ce31
Giridhar29 <karinkigiridhar@gmail.com>
2025-03-08 00:10:08 +0530

    day 1 project

---
+++ Day_1/Day1Projects/classobjectdemo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CubeDemo;
+
+namespace classobjectdemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class Employee
+        {
+            int salary;
+            int bonus;
+
+            public void totalsalary(int sal1, int bonus1)
+            {
+                this.salary = sal1;
+                this.bonus = bonus1;
+                MessageBox.Show("The total Salary is " + (salary + bonus));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.totalsalary(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CubeExample obj = new CubeExample();
+            MessageBox.Show("The cube  is " + obj.FindCube(4));
+        }
+    }
+}
